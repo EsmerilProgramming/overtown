6dcc6aac803aec2b331402d8eebc82bdd0b54b89
eprogramming <00hf11@gmail.com>
2014-07-20 15:23:39 -0300

    issue #40

--- src/main/java/org/esmerilprogramming/cloverx/http/converter/IntegerConverter.java
+++ src/main/java/org/esmerilprogramming/cloverx/http/converter/IntegerConverter.java
@@ -1,31 +1,32 @@
 package org.esmerilprogramming.cloverx.http.converter;
 
 import org.esmerilprogramming.cloverx.http.CloverXRequest;
+import org.jboss.logging.Logger;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * 
  * @author efraimgentil (efraim.gentil@gmail.com)
  */
 public class IntegerConverter implements ParameterConverter {
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> T translate(Class<T> clazz, String parameterName,
-			CloverXRequest cloverRequest) {
-		Object attribute = cloverRequest.getParameter(parameterName);
-		if(attribute != null){
-			String strVal = String.valueOf(attribute);
-			
-			try{
-				Integer i = Integer.parseInt(strVal);
-				return (T) i; 
-			}catch(NumberFormatException nfe){
-				Logger.getLogger(IntegerConverter.class.getName()).log(Level.SEVERE, nfe.getMessage());
-			}
-		}
-		return null;
-	}
+
+  private static final Logger LOGGER = Logger.getLogger(IntegerConverter.class);
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> T translate(Class<T> clazz, String parameterName, CloverXRequest cloverRequest) {
+    Object attribute = cloverRequest.getParameter(parameterName);
+    if (attribute != null) {
+      String strVal = String.valueOf(attribute);
+
+      try {
+        Integer i = Integer.parseInt(strVal);
+        return (T) i;
+      } catch (NumberFormatException nfe) {
+        LOGGER.error(nfe.getMessage());
+      }
+    }
+    return null;
+  }
 
 }
