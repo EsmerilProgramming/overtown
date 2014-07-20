9e5945b9e5268b70c62efaf208ce27c0ad1615c2
eprogramming <00hf11@gmail.com>
2014-07-20 18:44:05 -0300

    startup cleanup and readme update.

--- src/main/java/org/esmerilprogramming/cloverx/server/CloverX.java
+++ src/main/java/org/esmerilprogramming/cloverx/server/CloverX.java
@@ -2,59 +2,79 @@ package org.esmerilprogramming.cloverx.server;
 
 import io.undertow.Undertow;
 import io.undertow.Undertow.Builder;
+import io.undertow.server.handlers.PathHandler;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 import org.esmerilprogramming.cloverx.scanner.PackageScanner;
 import org.esmerilprogramming.cloverx.scanner.ScannerResult;
 import org.esmerilprogramming.cloverx.scanner.exception.PackageNotFoundException;
 import org.jboss.logging.Logger;
 
-public class CloverX {
+public final class CloverX {
 
   private static final Logger LOGGER = Logger.getLogger(CloverX.class);
-  
+
+  private Undertow server;
   private String host = "localhost";
   private int port = 8080;
 
   public CloverX() {
-
+    start();
   }
 
   public CloverX(int port) {
     this.port = port;
+    start();
   }
 
   public CloverX(String host) {
     this.host = host;
+    start();
   }
 
   public CloverX(int port, String host) {
     this.port = port;
     this.host = host;
+    start();
   }
 
-  private Undertow server;
-
-  public void start() throws PackageNotFoundException, IOException, NoSuchMethodException,
-      SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException,
-      InvocationTargetException {
-    ClassLoader classLoader = this.getClass().getClassLoader();
-    ScannerResult scan = new PackageScanner().scan("", classLoader);
-
-    Builder builder = Undertow.builder();
-    builder.addHttpListener(port, host);
-
-    if (!scan.getHandlers().isEmpty()) {
-      PathHandlerMounter mounter = new PathHandlerMounter();
-      builder.setHandler(mounter.mount(scan.getHandlers()));
-    }
-
-    server = builder.build();
+  public void start() {
+    
+    LOGGER.info("ignition...");
+    
+    server = createBuilder();
+    
     server.start();
     
     LOGGER.info("Enjoy it! http://" + host + ":" + port);
+  }
+  
+  private Undertow createBuilder() {
+    Builder builder = Undertow.builder();
+    builder.addHttpListener(port, host);
+    builder.setHandler(createHandler());
+    return builder.build();
+  }
+  
+  private PathHandler createHandler() {
+    ScannerResult scan = createScanner();
+    if (!scan.getHandlers().isEmpty()) {
+      PathHandlerMounter mounter = new PathHandlerMounter();
+      return mounter.mount(scan.getHandlers());
+    }
+    return null;
+  }
+  
+  private ScannerResult createScanner() {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    ScannerResult scan = null;
+    try {
+      scan = new PackageScanner().scan("", classLoader);
+    } catch (PackageNotFoundException | IOException e) {
+      LOGGER.error(e.getMessage());
+    }
+    return scan;
   }
 
   public Undertow getServer() {
@@ -65,8 +85,8 @@ public class CloverX {
     this.server = server;
   }
 
-  public static void main(String[] args) throws Exception {
-    new CloverX().start();
+  public static void main(String[] args) {
+    new CloverX();
   }
 
 }
