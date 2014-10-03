6b461fa6983dcb407ad5cd8d0ad3da965749b5ea
Efraim Gentil <efraim.gentil@gmail.com>
2014-10-02 23:41:11 -0300

    Cleanup and Controller as request scope

--- src/main/java/org/esmerilprogramming/cloverx/server/HandlerCreator.java
+++ src/main/java/org/esmerilprogramming/cloverx/server/HandlerCreator.java
@@ -28,92 +28,91 @@ import com.thoughtworks.paranamer.CachingParanamer;
 import com.thoughtworks.paranamer.Paranamer;
 
 public class HandlerCreator {
-  
+
   private Class<?> pageClass;
   private Method method;
   private List<Method> beforeExecutemethods;
   private Paranamer paranamer;
-  
+
   public HandlerCreator() {
     paranamer = new CachingParanamer(new BytecodeReadingParanamer());
   }
-  
-  public HandlerCreator forPageClass(Class<?> pageClass){
+
+  public HandlerCreator forPageClass(Class<?> pageClass) {
     this.pageClass = pageClass;
     return this;
   }
-  
-  public HandlerCreator withPathMethod(Method method){
+
+  public HandlerCreator withPathMethod(Method method) {
     this.method = method;
     return this;
   }
-  
-  public HandlerCreator withExecuteBeforeMethods(List<Method> methods){
+
+  public HandlerCreator withExecuteBeforeMethods(List<Method> methods) {
     this.beforeExecutemethods = methods;
     return this;
   }
-  
-  public HttpHandler mount(){
-    return createHandlerForPage(pageClass, method , beforeExecutemethods);
+
+  public HttpHandler mount() {
+    return createHandlerForPage(pageClass, method, beforeExecutemethods);
   }
-  
-  private HttpHandler createHandlerForPage(final Class<?> pageClass , final Method method , final List<Method> beforeExecutemethods){
+
+  private HttpHandler createHandlerForPage(final Class<?> pageClass, final Method method,
+      final List<Method> beforeExecutemethods) {
     final String[] parameterNames = paranamer.lookupParameterNames(method);
     Page methodPagePath = method.getAnnotation(Page.class);
     final String responseTemplate = methodPagePath.responseTemplate();
-    
+
     HttpHandler handler = null;
-    try {
-      handler = new HttpHandler() {
-        private final Logger LOGGER = Logger.getLogger(pageClass);
-        
-        private final Object newInstance = pageClass.getConstructor().newInstance();
-        final Map<String, GenericConverter<?>> converterMap = new ConverterMounterImpl().mountParameterConveters(method, parameterNames);
-        final Map<String, ParameterConverter> paramConverterMap = new ParameterConverterMounter().identifyParametersTranslators(parameterNames, method.getParameterTypes());
-        
-        public void handleRequest(HttpServerExchange exchange) throws Exception {
-            long beforeR = System.nanoTime();
-            try {
-              Class<?>[] parameterTypes = method.getParameterTypes();
-              CloverXRequest request = new CloverXRequest(exchange);
+    handler = new HttpHandler() {
+      private final Logger LOGGER = Logger.getLogger(pageClass);
 
-              for (Method method : beforeExecutemethods) {
-                method.invoke(newInstance, request);
-              }
-              request.addConverters(converterMap);
-              ParametersConverter translator = new ParametersConverter( paramConverterMap );
-              
-              Object[] parameters = translator.translateParameters(parameterNames, parameterTypes, request );
-              CoreClassInjector injector = new CoreClassInjectorImpl();
-              parameters =  injector.injectCoreInstances(parameterNames , parameters , parameterTypes, request);
-              
-              method.invoke(newInstance, parameters);
+      // private final Object newInstance = pageClass.getConstructor().newInstance();
+      final Map<String, GenericConverter<?>> converterMap = new ConverterMounterImpl()
+          .mountParameterConveters(method, parameterNames);
+      final Map<String, ParameterConverter> paramConverterMap = new ParameterConverterMounter()
+          .identifyParametersTranslators(parameterNames, method.getParameterTypes());
 
-              Response response = request.getResponse();
-              if (!Page.NO_TEMPLATE.equals(responseTemplate) && !response.isResponseSend()) {
-                 request.respondAsHttp();
-                 ((HttpResponse)request.getResponse() ).fowardTo( responseTemplate );  
-              }else{
-                //TODO should check json or xml response 
-                if(!response.isResponseSend()){
-                  response.close();
-                }
-              }
-            } catch (IllegalAccessException | IllegalArgumentException
-                | InvocationTargetException e) {
-              e.printStackTrace();
-              LOGGER.error(e.getMessage());
+      public void handleRequest(HttpServerExchange exchange) throws Exception {
+        Object newInstance = pageClass.getConstructor().newInstance();
+        try {
+          Class<?>[] parameterTypes = method.getParameterTypes();
+          CloverXRequest request = new CloverXRequest(exchange);
+
+          for (Method method : beforeExecutemethods) {
+            method.invoke(newInstance, request);
+          }
+          request.addConverters(converterMap);
+          ParametersConverter translator = new ParametersConverter(paramConverterMap);
+
+          Object[] parameters =
+              translator.translateParameters(parameterNames, parameterTypes, request);
+          CoreClassInjector injector = new CoreClassInjectorImpl();
+          parameters =
+              injector.injectCoreInstances(parameterNames, parameters, parameterTypes, request);
+
+          method.invoke(newInstance, parameters);
+
+          Response response = request.getResponse();
+          if (!Page.NO_TEMPLATE.equals(responseTemplate) && !response.isResponseSend()) {
+            request.respondAsHttp();
+            ((HttpResponse) request.getResponse()).fowardTo(responseTemplate);
+          } else {
+            // TODO should check json or xml response
+            if (!response.isResponseSend()) {
+              response.close();
             }
-            System.out.println( System.nanoTime() - beforeR  );
+          }
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+          e.printStackTrace();
+          LOGGER.error(e.getMessage());
         }
-      };
-    } catch (NoSuchMethodException | InstantiationException | IllegalAccessException
-        | InvocationTargetException e) {
-      e.printStackTrace();
-    }
-    
+      }
+    };
+
     CloverXSessionManager sessionManager = CloverXSessionManager.getInstance();
-    return new SessionAttachmentHandler( handler , sessionManager.getSessionManager(), sessionManager.getSessionConfig() );
+    return new SessionAttachmentHandler(handler, sessionManager.getSessionManager(),
+        sessionManager.getSessionConfig());
   }
-  
+
 }
